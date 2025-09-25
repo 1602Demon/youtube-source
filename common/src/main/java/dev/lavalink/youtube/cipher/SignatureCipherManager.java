cc06064215f12052f875aa8e956c09d70d476779
1602Demon <130319726+1602Demon@users.noreply.github.com>
2025-09-24 22:21:21 -0700

    Update SignatureCipherManager.java

--- common/src/main/java/dev/lavalink/youtube/cipher/SignatureCipherManager.java
+++ common/src/main/java/dev/lavalink/youtube/cipher/SignatureCipherManager.java
@@ -60,7 +60,7 @@ public class SignatureCipherManager {
   );
 
   private static final Pattern ACTIONS_PATTERN = Pattern.compile(
-      "var\\s+([$A-Za-z0-9_]+)\\s*=\\s*\\{" +
+      "var\\s+([$A-Za-z0-9_-]+)\\s*=\\s*\\{" +
           "\\s*" + VARIABLE_PART_OBJECT_DECLARATION + "\\s*:\\s*function\\s*\\([^)]*\\)\\s*\\{[^{}]*(?:\\{[^{}]*}[^{}]*)*}\\s*," +
           "\\s*" + VARIABLE_PART_OBJECT_DECLARATION + "\\s*:\\s*function\\s*\\([^)]*\\)\\s*\\{[^{}]*(?:\\{[^{}]*}[^{}]*)*}\\s*," +
           "\\s*" + VARIABLE_PART_OBJECT_DECLARATION + "\\s*:\\s*function\\s*\\([^)]*\\)\\s*\\{[^{}]*(?:\\{[^{}]*}[^{}]*)*}\\s*};");
