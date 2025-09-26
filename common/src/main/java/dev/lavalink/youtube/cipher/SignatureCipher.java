59252d7bb020b19b989b97940c5a7a063887d37f
1602Demon <130319726+1602Demon@users.noreply.github.com>
2025-09-25 22:57:38 -0700

    Update SignatureCipher.java

--- common/src/main/java/dev/lavalink/youtube/cipher/SignatureCipher.java
+++ common/src/main/java/dev/lavalink/youtube/cipher/SignatureCipher.java
@@ -3,6 +3,7 @@ package dev.lavalink.youtube.cipher;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.mozilla.javascript.NativeArray;
 
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
